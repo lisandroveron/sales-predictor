a16795c190cd755d6c7c6614e2a6b86ceb88fd2f
Lisandro Veron <lisandro.veron@hotmail.com>
2023-07-31 01:03:51 -0300

    Client -> Added: Strict Mode for React.

--- client/index.jsx
+++ client/index.jsx
@@ -1,5 +1,10 @@
+import React from "react";
 import {createRoot} from "react-dom/client";
 import {App} from "./src/App.jsx";
 
 const root = createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+);
